689f688b7f8823a061ffee692b2aecb14b71b8f8
Catherine Livingston <catherine.livingston@duke.edu>
2020-07-18 16:32:17 -0400

    Adding "Action" plugin

---
+++ plugins/actions.js
@@ -0,0 +1,20 @@
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+const methods = {
+    run: function(request, response) {
+         // Strip the trigger word from the response.
+        const message = "Here are some ways to get involved:/n - Text 'volunteer' to learn about our upcoming events/n - Text 'petition' to learn about a recent petition and sign it/n - Text 'donate' to find out how you can financially support the Okra Project"
+
+         // Create a new response object to send to Twilio.
+        const twiml = new MessagingResponse()
+
+        // Add the message to it, and send it back to Twilio.
+        twiml.message(message)
+        response.set('Content-Type', 'text/xml')
+        response.send(twiml.toString())      
+    },
+
+    meta: {
+        aliases: ['action', 'actions', 'Action', 'Actions', 'ACTION', 'ACTIONS']
+    }
+}
+module.exports = methods
