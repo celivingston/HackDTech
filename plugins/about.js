18f9426b69207f8156c6c43a7635e341a82d1151
aihirsch <aihirsch@yahoo.com>
2020-07-18 16:13:31 -0400

    fixed app.js

---
+++ plugins/about.js
@@ -0,0 +1,20 @@
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+const methods = {
+    run: function(request, response) {
+         // Strip the trigger word from the response.
+        const message = 'About the Okra Project'
+
+         // Create a new response object to send to Twilio.
+        const twiml = new MessagingResponse()
+
+        // Add the message to it, and send it back to Twilio.
+        twiml.message(message)
+        response.set('Content-Type', 'text/xml')
+        response.send(twiml.toString())      
+    },
+
+    meta: {
+        aliases: ['about', 'organization']
+    }
+}
+module.exports = methods
