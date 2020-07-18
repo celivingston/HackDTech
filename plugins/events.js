fa91fdd9240490bba93c39b42f82035274309e96
delaneydemark <delaney.demark@duke.edu>
2020-07-18 17:08:39 -0400

    create events.js
    created events file which returns information about the orgs events

---
+++ plugins/events.js
@@ -0,0 +1,20 @@
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+const methods = {
+    run: function(request, response) {
+         // Strip the trigger word from the response.
+        const message = 'The Okra Project has many connections with the theatre community and other artistic spaces. We use these connections to provide Black Trans people with completely free opportunities to view performances in some of the most renowned Off-Broadway houses. All attendees receive a chef-prepared  prepackaged meal along with their theatre ticket.'
+
+         // Create a new response object to send to Twilio.
+        const twiml = new MessagingResponse(message.conc)
+
+        // Add the message to it, and send it back to Twilio.
+        twiml.message(message)
+        response.set('Content-Type', 'text/xml')
+        response.send(twiml.toString())
+    },
+
+    meta: {
+        aliases: ['events', 'event', 'upcoming']
+    }
+}
+module.exports = methods
