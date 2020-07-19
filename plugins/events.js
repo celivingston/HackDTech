2f6d386db52a159dd8e0e8c6ebc948a778e0f77a
delaneydemark <delaney.demark@duke.edu>
2020-07-19 10:08:58 -0400

    added other keywords for events

--- plugins/events.js
+++ plugins/events.js
@@ -2,7 +2,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse
 const methods = {
     run: function(request, response) {
          // Strip the trigger word from the response.
-        const message = 'The Okra Project has many connections with the theatre community and other artistic spaces. We use these connections to provide Black Trans people with completely free opportunities to view performances in some of the most renowned Off-Broadway houses. All attendees receive a chef-prepared  prepackaged meal along with their theatre ticket.'
+        const message = 'The Okra Project has many connections with the theatre community and other artistic spaces. We use these connections to provide Black Trans people with completely free opportunities to view performances in some of the most renowned Off-Broadway houses. All attendees receive a chef-prepared  prepackaged meal along with their theatre ticket.\n\t-Text \'actions\' to learn more about how you can get involved.\n\t-Text \'hi\' for a list of all keywords.'
 
          // Create a new response object to send to Twilio.
         const twiml = new MessagingResponse(message.conc)
