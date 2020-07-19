e019a8964d7f8f68574b5196ad8faef934a8242a
Catherine Livingston <catherine.livingston@duke.edu>
2020-07-19 10:17:43 -0400

    Changing 'help' to 'hi'

--- plugins/donate.js
+++ plugins/donate.js
@@ -2,7 +2,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse
 const methods = {
     run: function(request, response) {
          // Strip the trigger word from the response.
-        const message = 'Follow this link and click \"Donate\" to support our cause \nhttps://www.artsbusinesscollaborative.org/asp-products/the-okra-project-sponsored-project/\n\t -Text \'actions\' to get a list of our action items\n\t -Text \'help\' for a list of all keywords'
+        const message = 'Follow this link and click \"Donate\" to support our cause \nhttps://www.artsbusinesscollaborative.org/asp-products/the-okra-project-sponsored-project/\n\t -Text \'actions\' to get a list of our action items\n\t -Text \'hi\' for a list of all keywords'
 
          // Create a new response object to send to Twilio.
         const twiml = new MessagingResponse(message.conc)
